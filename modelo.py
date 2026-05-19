5b86babd65ab3480b94e14039e261d57d5b68853
Citlaly Monserrat Diaz Roman <monserratdiazroman@gmail.com>
2026-05-19 10:03:54 -0600

    fix carga lazy modelo render

--- modelo.py
+++ modelo.py
@@ -1,21 +1,15 @@
 import pandas as pd
 import os
 
-print("CARGANDO MODELO...")
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ruta_csv = os.path.join(BASE_DIR, "Ropa.csv")
-
-try:
-    df = pd.read_csv(ruta_csv, encoding="utf-8")
-    print("CSV cargado correctamente")
-    print(df.head())
-except Exception as e:
-    print("ERROR CARGANDO CSV:", e)
-    df = pd.DataFrame(columns=["Prenda", "Imagen", "Color", "Estilo", "Genero"])
+def cargar_datos():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    ruta_csv = os.path.join(BASE_DIR, "Ropa.csv")
+    return pd.read_csv(ruta_csv)
 
 def recomendar(color, estilo, genero):
     
+    df = cargar_datos()
+
     resultado = df[
         (df["Color"] == color) &
         (df["Estilo"] == estilo) &
